d6316a9fd47c2d25e8e11012e7a16d2a8816ae1d
Austin Bonander <austin.bonander@gmail.com>
2014-11-18 16:40:11 -0800

    Minor correction

--- src/lib.rs
+++ src/lib.rs
@@ -43,8 +43,10 @@ impl ImageHash {
     }
 
     /// Calculate the Hamming distance between `self` and `other`,
-    /// then normalize it to `[0, 1]`, as a fraction of the total bits,
-    /// allowing comparisons independent of hash size.
+    /// then normalize it to `[0, 1]`, as a fraction of the total bits.
+    /// 
+    /// Roughly equivalent to the % difference between the two images,
+    /// represented as a decimal.
     ///
     /// See `ImageHash::dist()`.
     pub fn dist_ratio(&self, other: &ImageHash) -> f32 {
@@ -59,11 +61,11 @@ impl ImageHash {
     /// If `fast == true`, use a simple average of the pixels (faster, but less accurate). 
     /// Else, perform a Discrete Cosine Transform & Mean hash (slower, but more accurate).
     ///
-    /// Setting `fast == true` produces hashes that are really only useful for
+    /// Setting `fast = true` produces hashes that are really only useful for
     /// comparing *equality* and very *closely* similar images with minor edits or corrections. 
     /// Strong color/gamma correction will throw off the hash. 
     ///
-    /// In practice, on a fast computer, using `fast == slow` won't drastically increase the hash
+    /// In practice, on a fast computer, using `fast = false` won't drastically increase the hash
     /// time for a single image. In a program that processes many images at once, the bottleneck
     /// will likely be in loading and decoding the images, and not in the hash function.
     pub fn hash<Img: GenericImage<Rgba<u8>>>(img: &Img, hash_size: u32, fast: bool) -> ImageHash {
