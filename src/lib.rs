fd4927dcd4c6d2696506ceb7838cf90749ca83d7
Austin Bonander <austin.bonander@gmail.com>
2018-12-29 04:59:32 -0800

    fix root example

--- src/lib.rs
+++ src/lib.rs
@@ -12,22 +12,21 @@
 //! extern crate image;
 //! extern crate img_hash;
 //! 
-//! use img_hash::{ImageHash, HashType};
+//! use img_hash::{HasherConfig, HashAlg};
 //!
 //! fn main() {
 //!     let image1 = image::open("image1.png").unwrap();
 //!     let image2 = image::open("image2.png").unwrap();
 //!     
-//!     // These two lines produce hashes with 64 bits (8 ** 2),
-//!     // using the Gradient hash, a good middle ground between 
-//!     // the performance of Mean and the accuracy of DCT.
-//!     let hash1 = ImageHash::hash(&image1, 8, HashType::Gradient);
-//!     let hash2 = ImageHash::hash(&image2, 8, HashType::Gradient);
+//!     let hasher = HasherConfig::new().to_hasher();
+//!
+//!     let hash1 = hasher.hash_image(&image1);
+//!     let hash2 = hasher.hash_image(&image2);
 //!     
 //!     println!("Image1 hash: {}", hash1.to_base64());
 //!     println!("Image2 hash: {}", hash2.to_base64());
 //!     
-//!     println!("% Difference: {}", hash1.dist(&hash2));
+//!     println!("Hamming Distance: {}", hash1.dist(&hash2));
 //! }
 //! ```
 //! [1]: https://github.com/PistonDevelopers/image
