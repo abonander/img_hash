1966d50dfacf751f7b741ed85080f1a0a1b1d168
john <john@vehicle.software>
2021-05-06 10:43:54 -0700

    Improve hash_image tool

--- src/bin/hash_image.rs
+++ src/bin/hash_image.rs
@@ -5,21 +5,28 @@ extern crate image;
 
 use img_hash::HasherConfig;
 
-use std::env;
-
 fn main() -> Result<(), String> {
-    let args = env::args().collect::<Vec<_>>();
-    assert_eq!(args.len(), 2);
+    let matches = clap::App::new("hash_image")
+    .version("3.2.0")
+    .arg(
+        clap::Arg::with_name("IMAGE")
+            .required(true)
+            .index(1)
+            .help("Image file to calculate hash over.")
+    )
+    .get_matches();
 
-    let image = image::open(&args[1]).map_err(|e| {
-        format!("failed to open {}: {}", &args[1], e)
+    let image_file = matches.value_of("IMAGE").unwrap();
+
+    let image = image::open(image_file).map_err(|e| {
+        format!("failed to open {}: {}", image_file, e)
     })?;
 
     let hash = HasherConfig::new().hash_size(8, 8).to_hasher().hash_image(&image);
 
     let hash_str = hash.as_bytes().iter().map(|b| format!("{:02x}", b)).collect::<String>();
 
-    println!("{}: {}", &args[1], hash_str);
+    println!("{}: {}", image_file, hash_str);
 
     Ok(())
 }
