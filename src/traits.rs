2d203d69338ec4a6605af73d87dc6e55d6186ba0
Sebastian Dröge <sebastian@centricular.com>
2022-11-16 15:39:35 +0200

    Fix panic in Blockhash gauss_preproc mode
    It's not clear to me if this wants wrapping_sub or saturating_sub or
    abs_diff behaviour, but this implements what happened before in release
    mode where overflow panics are disabled: wrapping_sub.

--- src/traits.rs
+++ src/traits.rs
@@ -229,7 +229,9 @@ where
     P: Pixel<Subpixel = u8>,
 {
     fn diff_inplace(&mut self, other: &Self) {
-        self.iter_mut().zip(other.iter()).for_each(|(l, r)| *l -= r);
+        self.iter_mut().zip(other.iter()).for_each(|(l, r)| {
+            *l = l.wrapping_sub(*r);
+        });
     }
 }
 
